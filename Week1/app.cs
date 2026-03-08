542b2a6df55966f017e7207866919104a3747bf4
Chris Aguilar <111602643+caguilar59@users.noreply.github.com>
2026-03-07 16:31:47 -0800

    Initial commit

---
+++ Week1/app.cs
@@ -0,0 +1,9 @@
+using System;//Imports the system library
+
+class Week1 // Declares a class name called Week1
+{
+    static void Main(string[] args) // Main method: the entry point where the program starts running.
+    {
+        Console.WriteLine("Hello, World!"); //Writes "Hello, World!" in the console
+    }
+}
